7690f638374a6de10ccc07939d6d41b6f48e2d00
cebe.cc Hostmaster <hostmaster@cebe.cc>
2015-04-23 01:21:41 +0200

    writeable + remove sudo

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 echo "deb http://pkg.jenkins-ci.org/debian binary/" > /etc/apt/sources.list.d/jenkins.list
 apt-get update
 apt-get -y install jenkins
