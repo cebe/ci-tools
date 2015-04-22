c02014ee88c73096d1a771568ec7b873861258b1
Carsten Brandt <mail@cebe.cc>
2015-04-23 00:07:57 +0200

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+echo "deb http://pkg.jenkins-ci.org/debian binary/" > /etc/apt/sources.list.d/jenkins.list
+apt-get update
+apt-get -y install jenkins
+
+# allow jenkins to run docker
+adduser jenkins docker
+
+echo "install plugins:"
+echo "- Github"
+echo "- Github pull request"
